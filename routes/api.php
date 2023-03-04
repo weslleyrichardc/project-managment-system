f98b761c8e788e0bda4d6b0d96eea7abfb7e89fd
Weslley Richard <weslleyrichardc@gmail.com>
2023-03-04 18:20:35 -0300

    chore: Organize the api routes

--- routes/api.php
+++ routes/api.php
@@ -6,25 +6,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')
-    ->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-Route::controller(AuthController::class)
-    ->name('auth')
-    ->middleware('auth:api')
     ->group(function () {
-        Route::post('login', 'login')->withoutMiddleware('auth:api');
-        Route::post('register', 'register')->withoutMiddleware('auth:api');
-        Route::post('logout', 'logout');
-        Route::post('refresh', 'refresh');
-    });
+        Route::controller(AuthController::class)
+        ->name('auth')
+        ->group(function () {
+            Route::get('user', fn (Request $request) => $request->user());
+            Route::post('login', 'login')->withoutMiddleware('auth:api');
+            Route::post('register', 'register')->withoutMiddleware('auth:api');
+            Route::post('logout', 'logout');
+            Route::post('refresh', 'refresh');
+        });
 
-Route::controller(AddressController::class)
-    ->name('addresses')
-    ->middleware('auth:api')
-    ->group(function () {
-        Route::post('addresses', 'store');
-        Route::put('addresses/{address}', 'update');
-        Route::delete('addresses/{address}', 'destroy');
+        Route::controller(AddressController::class)
+            ->name('addresses')
+            ->group(function () {
+                Route::post('addresses', 'store');
+                Route::put('addresses/{address}', 'update');
+                Route::delete('addresses/{address}', 'destroy');
+            });
     });
